72a286d3aa19c2faf77d8dca4561ef9a71326980
vinnydsstos <vinistos@gmail.com>
2022-11-21 13:23:28 -0300

    :tada: Refactor the project into three new projects
    to make more sense (microservice), this commit refactor the main project into three different projects, being medical care attendance, medical care finances, and sales the modules created.

---
+++ MedicalCareFinances/src/main/java/com/csm/MedicalCareFinances/Application.java
@@ -0,0 +1,13 @@
+package com.csm.medicalcarefinances.MedicalCareFinances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalCareFinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalCareFinancesApplication.class, args);
+	}
+
+}
---
+++ MedicalCareSales/src/main/java/com/csm/MedicalCareSales/Application.java
@@ -0,0 +1,13 @@
+package com.csm.medicalcaresales.MedicalCareSales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalCareSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalCareSalesApplication.class, args);
+	}
+
+}
